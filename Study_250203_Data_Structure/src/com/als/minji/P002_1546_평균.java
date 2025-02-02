b2c45a04b9e77d07c5f2a662eacdbd975c87146a
MindySo <mindyso7151@gmail.com>
2025-02-02 21:20:39 +0900

    250202_P002_1546

--- Study_250203_Data_Structure/src/com/als/minji/P002_1546_평균.java
+++ Study_250203_Data_Structure/src/com/als/minji/P002_1546_평균.java
@@ -1,5 +1,26 @@
 package com.als.minji;
 
-public class P002_1546_평균 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class P002_1546_평균 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double[] nums = new double[N + 1];
+		for(int i = 0 ; i < N ; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+			nums[N] = Math.max(nums[N], nums[i]);
+		}
+		
+		double avg = 0;
+		for(int i = 0 ; i < N ; i++) {
+			avg += nums[i] / nums[N] * 100;
+		}
+		System.out.println(avg / N);
+	}
 }

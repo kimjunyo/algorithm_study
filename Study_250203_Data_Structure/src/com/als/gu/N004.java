6540b0a3c2d5da92aed0bcf659515b41898a4e16
김구 <skk3010g@gmail.com>
2025-02-03 20:54:31 +0900

    gu:update n004

--- Study_250203_Data_Structure/src/com/als/gu/N004.java
+++ Study_250203_Data_Structure/src/com/als/gu/N004.java
@@ -1,34 +1,45 @@
 package com.als.gu;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N004 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
 		int[][] matrix = new int[N][N];
-		int[][] sumMatrix = new int[N][N];
+		int[][] sumMatrix = new int[N+1][N+1];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
+//				for (int k = 0; k <= i; k++) {
+//					for (int l = 0; l <= j; l++) {
+//						sumMatrix[i][j] += matrix[k][l];
+//					}
+//				}
+			}
+		}
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				matrix[i][j] = sc.nextInt();
-				for (int k = 0; k <= i; k++) {
-					for (int l = 0; l <= j; l++) {
-						sumMatrix[i][j] += matrix[k][l];
-					}
-				}
+				sumMatrix[i+1][j+1] = sumMatrix[i][j+1] + sumMatrix[i+1][j] - sumMatrix[i][j] + matrix[i][j];
 			}
-			
 		}
 		
 		
 		for (int i = 0; i < M; i++) {
-			int x1 = sc.nextInt() - 1;
-			int y1 = sc.nextInt() - 1;
-			int x2 = sc.nextInt() - 1;
-			int y2 = sc.nextInt() - 1;
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken()) - 1;
+			int y1 = Integer.parseInt(st.nextToken()) - 1;
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
 			
 			System.out.println(sumMatrix[x2][y2] - sumMatrix[x1][y2] - sumMatrix[x2][y1] + sumMatrix[x1][y1]);
 		}

612bf6a6776b3e6b97ccc49f5ebf30b66f53c470
김구 <skk3010g@gmail.com>
2025-02-03 17:55:48 +0900

    Update Study_250203_Data_Structure/src/com/als/gu/N004.java

--- Study_250203_Data_Structure/src/com/als/gu/N004.java
+++ Study_250203_Data_Structure/src/com/als/gu/N004.java
@@ -9,26 +9,28 @@ public class N004 {
 		int M = sc.nextInt();
 		
 		int[][] matrix = new int[N][N];
+		int[][] sumMatrix = new int[N][N];
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				matrix[i][j] = sc.nextInt();
-			}
-		}
-		
-		for (int i = 0; i < M; i++) {
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
-			
-			int sum = 0;
-			for (int j = x1-1; j < x2; j++) {
-				for (int k = y1-1; k < y2; k++) {
-					sum += matrix[j][k];
+				for (int k = 0; k <= i; k++) {
+					for (int l = 0; l <= j; l++) {
+						sumMatrix[i][j] += matrix[k][l];
+					}
 				}
 			}
-			System.out.println(sum);
+			
+		}
+		
+		
+		for (int i = 0; i < M; i++) {
+			int x1 = sc.nextInt() - 1;
+			int y1 = sc.nextInt() - 1;
+			int x2 = sc.nextInt() - 1;
+			int y2 = sc.nextInt() - 1;
+			
+			System.out.println(sumMatrix[x2][y2] - sumMatrix[x1][y2] - sumMatrix[x2][y1] + sumMatrix[x1][y1]);
 		}
 	}
 }

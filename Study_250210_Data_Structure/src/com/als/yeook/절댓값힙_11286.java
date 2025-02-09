ec806d2fa23887cc5e8f258ad89cb281833f6509
yunyeook <dudhr17050@naver.com>
2025-02-09 17:10:28 +0900

    큐

--- Study_250210_Data_Structure/src/com/als/yeook/절댓값힙_11286.java
+++ Study_250210_Data_Structure/src/com/als/yeook/절댓값힙_11286.java
@@ -9,28 +9,27 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class 절댓값힙_11286 {
-	public static void main(String[] args) throws Exception{
-//		PriorityQueue<Integer> heap = new PriorityQueue<>(new CompareAbs());
-//		Integer pop = heap.poll();
-//		System.out.println(pop);
-
+	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 우선순위 큐만들고 절댓값을 기준으로 저장하기.
 		PriorityQueue<Integer> heap = new PriorityQueue<>(new CompareAbs());
 		StringBuilder result = new StringBuilder();
-		
+
 		int n = Integer.parseInt(st.nextToken());
-		for(int i = 0;i<n;i++) {
+		for (int i = 0; i < n; i++) {
 			int num = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-			if(num!=0) {
+			if (num != 0) {
 				heap.offer(num);
-			}else {
-				Integer pop =heap.poll();
-			if(pop!=null) result.append(pop).append("\n");
-			else result.append("0\n");
-			
+			} else {
+				Integer pop = heap.poll();
+				if (pop != null)
+					result.append(pop).append("\n");
+				else
+					result.append("0\n");
+
 			}
 		}
 		bw.write(result.toString().trim());
@@ -38,15 +37,19 @@ public class 절댓값힙_11286 {
 	}
 
 }
+
+//우선순위 큐의 인자로 넣을 Comparator구현한 클래스 만들기.
+//compare메서드 호출 결과를 기준으로 큐에 저장됨.
 class CompareAbs implements Comparator<Integer> {
 
 	@Override
 	public int compare(Integer a, Integer b) {
-		if(Math.abs(a)>Math.abs(b)) {
-			return 1; //양수이면 자리 바꿈. 
-		}else if(Math.abs(a)==Math.abs(b)) {
-			return a-b;
-		}else {
-			return -1; //음수이면 자리 그대로.
+		if (Math.abs(a) > Math.abs(b)) {
+			return 1; // 양수이면 자리 바꿈.
+		} else if (Math.abs(a) == Math.abs(b)) {
+			return a - b;// 같으면 음수가 앞이있도록 함.
+		} else {
+			return -1; // 음수이면 자리 그대로.
 		}
-	}}
+	}
+}

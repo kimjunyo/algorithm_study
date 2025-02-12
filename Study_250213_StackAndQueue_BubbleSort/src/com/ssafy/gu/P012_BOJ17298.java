e717b04d36cb073e99d57f61c32990831dbb148b
김구 <skk3010g@gmail.com>
2025-02-13 03:32:40 +0900

    Upload New File

---
+++ Study_250213_StackAndQueue_BubbleSort/src/com/ssafy/gu/P012_BOJ17298.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class P012_BOJ17298 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			stack.clear();
+			for (int j = 0; j < N; j++) {
+				stack.push(arr[j]);
+			}
+			int result = -1;
+			for (int j = i; j < N; j++) {
+				int pop = stack.pop();
+				if (arr[i] < pop) {
+					result = pop;
+				}
+			}
+			sb.append(result);
+			sb.append(" ");
+		}
+		System.out.print(sb);
+		
+	}
+}

e3e9990952aaaab8771fd813eaf5e3b6aba3781d
최시영 <poemzero@naver.com>
2025-02-12 09:38:58 +0900

    Upload New File

---
+++ Study_250213_StackAndQueue_BubbleSort/siyeong/오큰수구하기_17298.java
@@ -0,0 +1,40 @@
+package als;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 오큰수구하기_17298 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		Stack<Integer> stack = new Stack<Integer>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+				arr[stack.pop()] = arr[i];
+			}
+			stack.push(i);
+		}
+		
+		while (!stack.isEmpty() ) {
+			arr[stack.pop()] = -1;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i]).append(' ');
+		}
+		
+		System.out.println(sb);
+	}
+}

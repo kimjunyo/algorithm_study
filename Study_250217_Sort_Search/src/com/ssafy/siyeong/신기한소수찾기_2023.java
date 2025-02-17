18aa0e38c3bf3ab4093a51458cc1f59d176e51fe
최시영 <poemzero@naver.com>
2025-02-17 15:29:29 +0900

    Upload New File

---
+++ Study_250217_Sort_Search/src/com/ssafy/siyeong/신기한소수찾기_2023.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 신기한소수찾기_2023 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int num = 1;
+		for (int i = 1; i < N; i++) {
+			num *= 10;
+		}
+		// 포기.............
+	}
+}

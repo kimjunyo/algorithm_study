1eb2b50a6f97f4948abdbe0990a440b5e64d08c0
MindySo <mindyso7151@gmail.com>
2025-02-26 02:01:41 +0900

    1931_unsolved

--- Study_250224_Greedy/src/com/ssafy/minji/P35_1931_회의실배정하기.java
+++ Study_250224_Greedy/src/com/ssafy/minji/P35_1931_회의실배정하기.java
@@ -3,9 +3,7 @@ package com.ssafy.minji;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -17,39 +15,48 @@ public class P35_1931_회의실배정하기 {
 		int[] start = new int[N];
 		int[] end = new int[N];
 		int maxStart = 0;
-		int maxEnd = 0;
-		int MinEndStart = 0; 
-		int minEnd = Integer.MAX_VALUE;
-		for(int i = 0 ; i < N ; i++	) {
+//		int minEnd = Integer.MAX_VALUE;
+//		int maxEnd = 0;
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			start[i] = Integer.parseInt(st.nextToken());
 			end[i] = Integer.parseInt(st.nextToken());
-			if(maxStart < start[i]) {
+			if (maxStart < start[i]) {
 				maxStart = start[i];
 			}
-			if(maxEnd < end[i]) {
-				maxEnd = end[i];
-			}
-			if(minEnd < end[i]) {
-				minEnd = end[i];
-				MinEndStart = start[i];
-			}
+//			if(maxEnd < end[i]) {
+//				maxEnd = end[i];
+//			}
+//			if(minEnd > end[i]) {
+//				minEnd = end[i];
+//			}
 		}
+
 		// 끝나는 시간 기준으로 리스트
-		PriorityQueue<Integer>[] pqArr = new PriorityQueue[maxEnd + 1];
-		for(int i = 0 ; i < maxEnd + 1 ; i++) {
+		PriorityQueue<Integer>[] pqArr = new PriorityQueue[end[end.length - 1] + 1];
+		for (int i = 0; i < end[end.length - 1] + 1; i++) {
 			pqArr[i] = new PriorityQueue<Integer>();
 		}
-		for(int i = 0 ; i < N ; i++) {
+		for (int i = 0; i < N; i++) {
 			pqArr[end[i]].add(start[i]);
 		}
-		
-		int now = maxEnd;
-		while(now <= maxStart) {
-//			pqArr[now] = 
+
+		Arrays.sort(end);
+		int idx = 0;
+		int cnt = 1;
+		pass:
+		while (end[idx] <= maxStart) {
+			for (int endIdx = idx + 1; endIdx < end.length - 1; endIdx++) {
+				for (int startTime : pqArr[end[endIdx]]) {
+					if (startTime >= end[idx]) {
+						idx = endIdx;
+						cnt++;
+						continue pass;
+					}
+				}
+			}
+			break;
 		}
-		
-		
-		
+		System.out.println(cnt);
 	}
 }

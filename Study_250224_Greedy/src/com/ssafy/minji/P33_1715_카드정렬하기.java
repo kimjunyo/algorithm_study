d5663ca17771391da9b2b5cfcdab40f5c93946f7
unknown <qhdrl527@naver.com>
2025-02-25 17:58:29 +0900

    250225_1715_solved

--- Study_250224_Greedy/src/com/ssafy/minji/P33_1715_카드정렬하기.java
+++ Study_250224_Greedy/src/com/ssafy/minji/P33_1715_카드정렬하기.java
@@ -5,39 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class P33_1715_카드정렬하기 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine().trim());
-		int[] arr = new int[N];
-		for(int i = 0 ; i < N ; i++) {
-			arr[i] = Integer.parseInt(br.readLine().trim());
+		PriorityQueue<Integer> que = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
+			que.add(Integer.parseInt(br.readLine().trim()));
+		}
+		int sum = 0;
+		int ans = 0;
+		while (que.size() > 1) {
+			sum = que.poll() + que.poll();
+			ans += sum;
+			que.add(sum);
 		}
 
-		if(N == 1) {
-			System.out.println(arr[0]);
-			return;
-		}else if(N == 2) {
-			System.out.println(arr[0] + arr[1]);
-			return;
-		}
-		
-		Arrays.sort(arr);
-		Queue<Integer> que = new LinkedList<Integer>();
-		for(int i = 0 ; i < N ; i++) {
-			que.add(arr[i]);
-		}
-		
-		int sum = que.poll() + que.poll();
-		int temp = 0;
-		for(int i = 2 ; i < N ; i++) {
-			temp = que.poll();
-			if(que.peek() < sum) {
-				
-			}
-		}
-		System.out.println(sum);
+		System.out.println(ans);
 	}
 }
--- Study_250224_Greedy/src/com/ssafy/minji/P35_1931_회의실배정하기.java
+++ Study_250224_Greedy/src/com/ssafy/minji/P35_1931_회의실배정하기.java
@@ -1,5 +1,55 @@
 package com.ssafy.minji;
 
-public class P35_1931_회의실배정하기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
+public class P35_1931_회의실배정하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int N = Integer.parseInt(br.readLine().trim());
+		int[] start = new int[N];
+		int[] end = new int[N];
+		int maxStart = 0;
+		int maxEnd = 0;
+		int MinEndStart = 0; 
+		int minEnd = Integer.MAX_VALUE;
+		for(int i = 0 ; i < N ; i++	) {
+			st = new StringTokenizer(br.readLine());
+			start[i] = Integer.parseInt(st.nextToken());
+			end[i] = Integer.parseInt(st.nextToken());
+			if(maxStart < start[i]) {
+				maxStart = start[i];
+			}
+			if(maxEnd < end[i]) {
+				maxEnd = end[i];
+			}
+			if(minEnd < end[i]) {
+				minEnd = end[i];
+				MinEndStart = start[i];
+			}
+		}
+		// 끝나는 시간 기준으로 리스트
+		PriorityQueue<Integer>[] pqArr = new PriorityQueue[maxEnd + 1];
+		for(int i = 0 ; i < maxEnd + 1 ; i++) {
+			pqArr[i] = new PriorityQueue<Integer>();
+		}
+		for(int i = 0 ; i < N ; i++) {
+			pqArr[end[i]].add(start[i]);
+		}
+		
+		int now = maxEnd;
+		while(now <= maxStart) {
+//			pqArr[now] = 
+		}
+		
+		
+		
+	}
 }

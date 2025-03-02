36e95979f8070b13ab6409140e0df452b5c165c2
MindySo <mindyso7151@gmail.com>
2025-03-02 13:26:17 +0900

    250302_1850_solved

--- Study_250227_Number_Theory/src/com/ssafy/minji/P043_1850_최대공약수.java
+++ Study_250227_Number_Theory/src/com/ssafy/minji/P043_1850_최대공약수.java
@@ -1,5 +1,40 @@
 package com.ssafy.minji;
 
-public class P043_1850_최대공약수 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class P043_1850_최대공약수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+		long temp = 0;
+		if(M > N) {
+			temp = N;
+			N = M;
+			M = temp;
+		}
+		
+//		long n = makeNum(N);
+//		long m = makeNum(M);
+		while(true) {
+			if(N % M == 0) {
+				break;
+			}else {
+				temp = N;
+				N = M;
+				M = temp % N;
+			}
+			
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < M ; i++) {
+			sb.append("1");
+		}
+		System.out.println(sb);
+	}
+	
 }

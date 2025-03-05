45a7b9a2d5da3f53a567d90868967130765c2180
김구 <skk3010g@gmail.com>
2025-03-06 08:56:51 +0900

    Upload New File

---
+++ Study_250306_DFS_BFS/src/com/ssafy/gu/BOJ18352.java
@@ -0,0 +1,47 @@
+package com.ssafy.gu;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ18352 {
+//	static int N;
+//	static int M;
+//	static int K;
+//	static int X;
+//	static ArrayList<Integer>[] doro;
+//	static Queue<Integer> q;
+//	static boolean[] visited;
+//	
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		N = sc.nextInt();
+//		M = sc.nextInt();
+//		K = sc.nextInt();
+//		X = sc.nextInt();
+//		doro = new ArrayList[N+1];
+//		visited = new boolean[N+1];
+//		for (int i = 0; i < M; i++) {
+//			doro[sc.nextInt()].add(sc.nextInt());
+//		}
+//		
+//		q = new LinkedList<>();
+//		q.add(X);
+//		visited[X] = true;
+//		int cnt = 0;
+//		while (!q.isEmpty()) {
+//			if (cnt == K) {
+//				
+//			}
+//			
+//			for (int v : doro[q.poll()]) {
+//				if (!visited[v]) q.add(v);
+//				visited[v] = true;
+//			}
+//			cnt++;
+//		}
+//
+//	}
+
+}

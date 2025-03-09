a50561c44ea3d3d1c5b228ec512638e899fcdc0b
김구 <skk3010g@gmail.com>
2025-03-10 08:56:08 +0900

    Upload New File

---
+++ Study_250310_/src/com/ssafy/gu/BOJ2251.java
@@ -0,0 +1,40 @@
+package com.ssafy.gu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ2251 {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int[] bottles = new int[3];
+//
+//		for (int i = 0; i < 3; i++) {
+//			bottles[i] = sc.nextInt();
+//		}
+//		
+//		if (bottles[2] > bottles[1] && bottles[2] > bottles[0]) {
+//			Arrays.sort(bottles);
+//			int a = bottles[0];
+//			int b = bottles[1];
+//			int c = bottles[2];
+//			System.out.println((c-b)+" "+(c-a)+" "+a+" "+" "+b+" "+c);
+//		}
+//		if (bottles[2] > bottles[1] && bottles[2] < bottles[0]) {
+//			Arrays.sort(bottles);
+//			int a = bottles[0];
+//			int b = bottles[1];
+//			System.out.println((b-a)+" "+a+" "+b);
+//		}
+//		if (bottles[2] < bottles[1] && bottles[2] < bottles[0]) {
+//			System.out.println(0 + " " + bottles[2]);
+//		}
+//		if (bottles[0] == bottles[1] && bottles[2] > bottles[1]) {
+//			int b = bottles[1];
+//			int c = bottles[2];
+//			System.out.println((c-b)+" "+b+" "+c);
+//		}
+	}
+}

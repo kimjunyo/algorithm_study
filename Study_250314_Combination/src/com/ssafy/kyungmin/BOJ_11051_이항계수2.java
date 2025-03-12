c23b05a85e83a06d685b9874a008c96cae47b540
kyngmn <kmoffi@naver.com>
2025-03-12 21:57:43 +0900

    feat: 이항계수 이름 변경 & 암호만들기

--- Study_250314_Combination/src/com/ssafy/kyungmin/BOJ_11051_이항계수2.java
+++ Study_250314_Combination/src/com/ssafy/kyungmin/BOJ_11051_이항계수2.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ_11051_이항계수2 {
 	static int[][] memo;
 	static int MOD = 10007;
 

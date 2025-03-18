64ebffb35e4db51a5f6ae639848916be9ee52fb7
mindyso <mindyso7151@gmail.com>
2025-03-18 12:11:01 +0900

    250318_11726_solved

--- Study_250320_DP/src/com/ssafy/minji/P11726_2xn타일링.java
+++ Study_250320_DP/src/com/ssafy/minji/P11726_2xn타일링.java
@@ -1,5 +1,33 @@
 package com.ssafy.minji;
 
-public class P11726_2xn타일링 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class P11726_2xn타일링 {
+	static int N;
+	static int k;
+	static long[] arr;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		k = 10007;
+		N = Integer.parseInt(br.readLine().trim());
+		arr = new long[N + 1];
+		arr[1] = 1;
+		if (N > 1) {
+			arr[2] = 2;
+			if (N > 2)
+				fibo(3);
+		}
+		System.out.println(arr[N]);
+	}
+
+	static void fibo(int idx) {
+		if (idx == N + 1) {
+			return;
+		}
+		arr[idx] = (arr[idx - 2] + arr[idx - 1]) % k;
+		fibo(idx + 1);
+	}
 }

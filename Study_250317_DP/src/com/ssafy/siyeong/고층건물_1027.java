dc3c1d41764530bde94a0a70fcd2fd316e0738dd
최시영 <poemzero@naver.com>
2025-03-19 09:07:20 +0900

    Add new file

---
+++ Study_250317_DP/src/com/ssafy/siyeong/고층건물_1027.java
@@ -0,0 +1,16 @@
+package com.ssafy.siyeong;
+
+import java.util.Scanner;
+
+public class 고층건물_1027 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		//모르겠어요.......
+		//기울기???
+	}
+}

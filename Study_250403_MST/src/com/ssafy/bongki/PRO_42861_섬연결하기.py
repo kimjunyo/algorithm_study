b01a4a3280633acc1de3baae5de17432ed899828
BONG-KI <qhdrl527@naver.com>
2025-04-03 08:59:32 +0900

    feat : update py file

--- Study_250403_MST/src/com/ssafy/bongki/PRO_42861_섬연결하기.py
+++ Study_250403_MST/src/com/ssafy/bongki/PRO_42861_섬연결하기.py
@@ -1,6 +1,6 @@
 def find(parent, x):
     if parent[x] != x:
-        parent[x] = find(parent, parent[x])  # 경로 압축
+        parent[x] = find(parent, parent[x])
     return parent[x]
 
 def union(parent, rank, a, b):
@@ -19,7 +19,6 @@ def union(parent, rank, a, b):
     return False
 
 def solution(n, costs):
-    # 1. 간선을 비용 기준으로 정렬
     costs.sort(key=lambda x: x[2])
     
     parent = [i for i in range(n)]
@@ -27,12 +26,11 @@ def solution(n, costs):
     total_cost = 0
     edge_count = 0
     
-    # 2. 크루스칼 알고리즘 적용
     for a, b, cost in costs:
-        if union(parent, rank, a, b):  # 연결 성공 시 비용 추가
+        if union(parent, rank, a, b):  
             total_cost += cost
             edge_count += 1
-            if edge_count == n - 1:  # MST 완성
+            if edge_count == n - 1:
                 break
                 
     return total_cost

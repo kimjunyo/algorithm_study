9d082232df44918e931c648b0a439653948f9485
BONG-KI <qhdrl527@naver.com>
2025-05-15 09:25:36 +0900

    Update BOJ_12865_평범한배낭.py

--- Study_250515_CodingTest10/src/com/ssafy/bongki/BOJ_12865_평범한배낭.py
+++ Study_250515_CodingTest10/src/com/ssafy/bongki/BOJ_12865_평범한배낭.py
@@ -1,1 +1,17 @@
 
+import sys
+input = sys.stdin.readline
+#n개 물건과 k개의 최대무게 배낭
+n, k = map(int, input().split())
+items = []
+
+#각물건의 무게와 해피정도 v
+for _ in range(n):
+    w, v = map(int, input().split())
+    items.append((w, v))
+
+#k무게까지의 행복정도 미리 최대크기
+dp =[0]*(1000001)
+  
+for i in range(1,k+1):
+  dp[i] = dp[i-1] + weight[i]
